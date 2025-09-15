88f491e21b3e7501735d121d4d5e50498b0aa310
kaikez <kaike.ribeiro@aluno.senai.br>
2025-09-15 10:46:09 -0300

    feat:cria arquivo select

--- 01-insert-artists.sql
+++ 01-insert-artists.sql
@@ -10,3 +10,11 @@ CREATE TABLE ARTISTAS (
 );
 
 SELECT *TABLE
+
+INSERT INTO ARTISTAS (id,NOME, TIPO , GENERO_MUSICAL , PAIS_ORIGEM , ANO_INICIO ,  MUSICA_MAIS_OUVIDA , ATIVO)
+VALUES
+(1,'drake','solo','trap','canada',2006,'gods s plain',TRUE);
+(2,'justin bieber','solo','pop','canada',2009,'sorry',TRUE)
+(3,'kendrick lamar','solo','rap','EUA',1995,'that not like us',TRUE)
+(4,'Notorius big','solo','rap','EUA',1993,'juicy',false)
+(5,'tupac','solo','rap','EUA',1987,'hit em up',false)
