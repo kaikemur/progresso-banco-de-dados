bae8d4f3e5b7115527ea930ed726baecccef37a9
kaikez <kaike.ribeiro@aluno.senai.br>
2025-09-22 14:41:12 -0300

    feat:cria comandos iniciais de update

---
+++ 03-update.sql
@@ -0,0 +1,13 @@
+SELECT * FROM usuarios;
+
+UPDATE usuarioS SET username  = 'ProGamer2025'
+WHERE username = 'NoobMaster';
+
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET pontos = pontos + 1000;
+
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET email = 'main@gmail.com'
+WHERE email = 'master@email.com';
